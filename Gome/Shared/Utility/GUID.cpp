fde6b6a3e170da31c69d364e17b876027afcecb0
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-03-13 22:41:17 +0200

    fixed GUID class

--- Gome/Shared/Utility/GUID.cpp
+++ Gome/Shared/Utility/GUID.cpp
@@ -5,20 +5,31 @@ namespace Shared {
 	namespace Utility {
 		GUID::GUID()
 			: mStatus(::UuidCreate(&mUUID)),
-			mIsGlobal(mStatus == RPC_S_UUID_LOCAL_ONLY) {
+			mIsGlobal(mStatus != RPC_S_UUID_LOCAL_ONLY) {
+		}
+
+		GUID::GUID(const GUID& guid) {
+			*this = guid;
+		}
+
+		GUID& GUID::operator=(const GUID& right) {
+			mUUID = right.mUUID;
+			mIsGlobal = right.mIsGlobal;
+			mStatus = ::UuidToString(&mUUID, &mUUIDString);
+			mUUIDStringTryCreate = true;
+
+			return *this;
 		}
 
 		GUID::~GUID() {
-			if (mUUIDString && IsGood()) {
-				::RpcStringFree(&mUUIDString);
-			}
+			Uninitialize();
 		}
 
 		bool GUID::IsGlobal() const {
 			return mIsGlobal;
 		}
 
-		const ::RPC_WSTR& GUID::GetStr() {
+		const GUID::RPC_STR& GUID::GetStr() {
 			if (!mUUIDString && IsGood()) {
 				mStatus = ::UuidToString(&mUUID, &mUUIDString);
 				mUUIDStringTryCreate = true;
@@ -27,16 +38,29 @@ namespace Shared {
 			return IsGood() ? mUUIDString : mUUIDStringDefault;
 		}
 
-		wstring GUID::GetString() {
-			return wstring(reinterpret_cast<const wchar_t*>(GetStr()));
-		}
-
-		const::UUID& GUID::GetUUID() const {
+		const ::UUID& GUID::GetUUID() const {
 			return IsGood() ? mUUID : mUUIDDefault;
 		}
 
 		void GUID::SetUUID(const ::UUID& uuid) {
+			Reset();
+
 			mUUID = uuid;
+		}
+
+		void GUID::Uninitialize() {
+			if (mUUIDString && IsGood()) {
+				::RpcStringFree(&mUUIDString);
+				mUUIDString = nullptr;
+			}
+		}
+
+		void GUID::Reset() {
+			Uninitialize();
+
+			mStatus = RPC_S_OK;
+			mIsGlobal = false;
+			mUUIDStringTryCreate = false;
 		}
 
 		bool GUID::IsGood() const {
--- Gome/Shared/Utility/GUID.h
+++ Gome/Shared/Utility/GUID.h
@@ -3,28 +3,39 @@
 namespace Shared {
 	namespace Utility {
 		class GUID {
+#ifdef UNICODE
+			typedef ::RPC_WSTR RPC_STR;
+#else
+			typedef ::RPC_CSTR RPC_STR;
+#endif // UNICODE
+
 		public:
 			static const size_t GUID_SIZE = 16;
 
 			GUID();
+			GUID(const GUID& guid);
 			~GUID();
 
 			bool IsGlobal() const;
 
-			const ::RPC_WSTR& GetStr();
-			wstring GetString();
+			const RPC_STR& GetStr();
 
 			const ::UUID& GetUUID() const;
 			void SetUUID(const ::UUID& uuid);
 
+			GUID& operator=(const GUID& right);
+
 		private:
 			bool IsGood() const;
+
+			void Uninitialize();
+			void Reset();
 
 			::UUID mUUID {};
 			static inline ::UUID mUUIDDefault {};
 
-			::RPC_WSTR mUUIDString {};
-			static inline ::RPC_WSTR mUUIDStringDefault = (::RPC_WSTR)TEXT("");
+			RPC_STR mUUIDString {};
+			static inline RPC_STR mUUIDStringDefault = (RPC_STR)TEXT("");
 
 			::RPC_STATUS mStatus {};
 			bool mIsGlobal {};
